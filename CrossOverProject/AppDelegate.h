f7346131b1a2d3148ba2d53c372ea8f63a82e6cd
osamagucCitySoc <osamarabie@osamas-macbook-pro.local>
2016-02-15 13:04:21 +0300

    Initial Commit

---
+++ CrossOverProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrossOverProject
+//
+//  Created by Osama Rabie on 2/15/16.
+//  Copyright © 2016 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
